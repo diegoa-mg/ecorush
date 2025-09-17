7d8a4513b03298327a964941f6e719ed2ccea25c
Diego Alejandro <ddiegoa.mg@gmail.com>
2025-09-17 16:19:02 -0600

    Primera version: fondo, titulo

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-# Simple pygame program
+import pygame 
 
-# Import and initialize the pygame library
-import pygame
-pygame.init()
+successes, failures = pygame.init() 
+print("{0} successes and {1} failures".format(successes, failures))   
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("EcoRush!")
+clock = pygame.time.Clock() 
+FPS = 60  # Frames per second. &
 
-# Run until the user asks to quit
+# BLACK = (0, 0, 0) 
+WHITE = (255, 255, 255) 
+# RED = (255, 0, 0), GREEN = (0, 255, 0), BLUE = (0, 0, 255).  
+
+# Cargar fondos y botones
+background = pygame.image.load("C:/Users/ddieg/Documents/ECORUSH/ecorush/assets/imglimpia_definitiva.png")
+titulo = pygame.image.load("C:/Users/ddieg/Documents/ECORUSH/ecorush/assets/titulo.png")
+botoninicio = pygame.image.load("C:/Users/ddieg/Documents/ECORUSH/ecorush/assets/botoninicio.png")
+botonconfig = pygame.image.load("C:/Users/ddieg/Documents/ECORUSH/ecorush/assets/botonconfig.png")
+botontuto = pygame.image.load("C:/Users/ddieg/Documents/ECORUSH/ecorush/assets/botontuto.png")
+
+# Escalar
+background = pygame.transform.scale(background, (1280, 720))  # Ajustar al tamaño de ventanas
+titulo = pygame.transform.scale(titulo, (1118, 178))
+# botoninicio = pygame.transform.scale(titulo, ())
+botonconfig = pygame.transform.scale(botonconfig, (334.66, 85.33))
+botontuto = pygame.transform.scale(botontuto, (334.66, 85.33))
+
 running = True
-while running:
+while True:     
+    clock.tick(FPS)      
+    for event in pygame.event.get():         
+        if event.type == pygame.QUIT:             
+            running = False              
+    
+    screen.blit(background, (0, 0))             
+    screen.blit(titulo, (81, 65)) 
+    # screen.blit(botoninicio, ())
+    screen.blit(botonconfig, (157.7, 500))
+    screen.blit(botontuto, (792.7, 500))
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    # Flip the display
     pygame.display.flip()
-
-# Done! Time to quit.
-pygame.quit()
+    
